5cb5f33d0b8470ff495108b69f7fe4f061df2511
szymonln <szymonln@gmail.com>
2018-11-20 12:10:35 +0100

    Vasic LU decomposition

--- gauss-car-distr.py
+++ gauss-car-distr.py
@@ -7,13 +7,16 @@ def gauss_jordan(m, eps=1.0/(10**10)):
 
   for y in range(0, h):
     mx_pivot_row = y
+
     for y2 in range(y+1, h):
       if abs(m[y2][y]) > abs(m[mx_pivot_row][y]):
         mx_pivot_row = y2
     (m[y], m[mx_pivot_row]) = (m[mx_pivot_row], m[y])
+
     if abs(m[y][y]) <= eps:
       print("It happens to be a singular matrix")
       return False
+
     for y2 in range(y+1, h):
       c = m[y2][y] / m[y][y]
       for x in range(y, w):
@@ -21,6 +24,7 @@ def gauss_jordan(m, eps=1.0/(10**10)):
 
   for y in range(h-1, 0-1, -1):
     c  = m[y][y]
+
     for y2 in range(0, y):
       for x in range(w-1, y-1, -1):
         m[y2][x] -= m[y][x] * m[y2][y] / c
@@ -76,7 +80,7 @@ print(equation)
 G = nx.DiGraph()
 G.add_edges_from(edges)
 pos = nx.spring_layout(G)
-nx.draw_networkx_nodes(G, pos, cmap=plt.get_cmap('jet'), node_size=500)
+nx.draw_networkx_nodes(G, pos, node_size=500)
 nx.draw_networkx_labels(G, pos)
 nx.draw_networkx_edges(G, pos, edgelist=edges, edge_color='r', arrows=True)
 
---
+++ lab6.py
@@ -0,0 +1,24 @@
+import numpy as np
+from scipy import linalg
+
+A = np.array(
+    [[1.0, 3.0, 4.0, 2.0],
+    [3.0, 0.0, -2.0, 1.0],
+    [8.0, 10.0, 22.0, -5.0],
+    [0.0, 3.0, 12.0, 1.0]]
+    )
+
+b = np.array(
+    [6.0,
+     12.0,
+    -10.0,
+     5.0]
+    )
+
+P, L, U = linalg.lu(A)
+
+print(A.dot(b))
+
+
+
+
